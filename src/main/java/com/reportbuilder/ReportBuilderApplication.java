dba91d50542cba0dd131588aea3df762273e22b8
Craig <craig_t_bridges@homedepot.com>
2017-01-31 14:19:17 -0500

    Initial Commit

---
+++ src/main/java/com/reportbuilder/ReportBuilderApplication.java
@@ -0,0 +1,12 @@
+package com.reportbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportBuilderApplication.class, args);
+	}
+}
